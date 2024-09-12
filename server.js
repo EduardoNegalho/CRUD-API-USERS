9f50e6681b123d02956be676af57dfcf24a77d72
Eduardo Marques <eduardomar1703@gmail.com>
2024-09-12 08:09:47 -0300

    feat: modifica a rota GET para verificar se há usuários e adiciona a rota POST para criar novos usuários

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const app = express();
 // Possibilita o uso do JSON
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Está ok!')
+const users = [];
+
+app.get('/users', (req, res) => {
+    // Verifica se existe usuários cadastrados
+    if (users.length === 0) {
+        console.log('Nenhum usuário encontrado');
+        return res.status(404).json({message: "Nenhum usuário encontrado"});
+    }
+
+    res.status(200).json(users);
 })
 
+app.post('/users', (req, res) => {
+    users.push(req.body); // => adiciona um novo usuário ao array de users
 
+    // Envia um status de criação, e retorna o usuário criado
+    res.status(201).json(req.body);
+})
 
 
 // Inicia a aplicação na porta 3000, essa informação sempre deve está no final
